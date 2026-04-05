aaa9a6505edbd9d96689efb3a01dd62ec42ad9da
stpkn <stepkina.maria@outlook.com>
2026-04-06 01:28:27 +0300

    feat: backend на SQLAlchemy + миграции Alembic + seed данные

---
+++ backend/app/schemas.py
@@ -0,0 +1,13 @@
+﻿from pydantic import BaseModel
+from typing import List, Optional
+
+class LoginRequest(BaseModel):
+    login: str
+    password: str
+
+class StudentCreate(BaseModel):
+    first_name: str
+    last_name: str
+    login: Optional[str] = None
+    password: Optional[str] = None
+    course_ids: List[int] = []
